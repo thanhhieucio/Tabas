560ccac2e673f2b996d0665c6ba619240158f487
Bui Thanh Hieu <thanhhieucio@gmail.com>
2020-03-26 15:13:15 +0700

    Hi Ubuntu

--- For odoo/Odoo 12/server/addons/Tabas/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import sanpham
-from . import nhomsanpham
-from . import donvitinh
-from . import khachhang
-from . import khuvuc
-from . import noidungthuchi
-# tên các file python trong Models
